5503c8344ccedca5c161b2e31234f39c2e676742
Zhao Tang <zhao.tang@rice.edu>
2021-04-28 18:33:53 -0500

    MaxCoins.java

---
+++ DP/MaxCoins.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+import jdk.incubator.jpackage.main.Main;
+
+public class MaxCoins {
+    public int maxCoins(int[] nums) {
+
+        // reframe the problem
+        int n = nums.length + 2;
+        int[] new_nums = new int[n];
+
+        for(int i = 0; i < nums.length; i++){
+            new_nums[i+1] = nums[i];
+        }
+
+        new_nums[0] = new_nums[n - 1] = 1;
+
+        // cache the results of dp
+        int[][] memo = new int[n][n];
+
+        // find the maximum number of coins obtained from adding all balloons from (0, len(nums) - 1)
+        return dp(memo, new_nums, 0, n - 1);
+    }
+
+    public int dp(int[][] memo, int[] nums, int left, int right) {
+        // no more balloons can be added
+        if (left + 1 == right) return 0;
+
+        // we've already seen this, return from cache
+        if (memo[left][right] > 0) return memo[left][right];
+
+        // add each balloon on the interval and return the maximum score
+        int ans = 0;
+        for (int i = left + 1; i < right; ++i)
+            ans = Math.max(ans, nums[left] * nums[i] * nums[right]
+            + dp(memo, nums, left, i) + dp(memo, nums, i, right));
+        // add to the cache
+        memo[left][right] = ans;
+        return ans;
+    }
+    public static void main(String[] args) {
+        MaxCoins sol = new MaxCoins();
+        int[] nums;
+        int res;
+
+        nums = new int[]{3,1,5,8};
+        res = sol.maxCoins(nums);
+        System.out.println("Input  : " + Arrays.toString(nums));
+        System.out.println("Output : " + res);
+
+        nums = new int[]{1,5};
+        res = sol.maxCoins(nums);
+        System.out.println("Input  : " + Arrays.toString(nums));
+        System.out.println("Output : " + res);
+    }    
+}
