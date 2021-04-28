8dbf0debe4687410ebe9fb0e68d6213d21093584
zt5.rice <zt5.rice@gmail.com>
2021-04-28 14:49:10 +0100

    MaxSubArray.java

---
+++ DP/MaxSubArray.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int glbmx = nums[0];
+        int cur = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (cur < 0) {
+                cur = nums[i];
+            } else {
+                cur = cur + nums[i];
+            }
+            glbmx = Math.max(glbmx, cur);
+        }
+        return glbmx;
+    }   
+    public static void main(String[] args) {
+        MaxSubArray sol = new MaxSubArray();
+        int[] nums;
+        int res;
+
+        nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        res = sol.maxSubArray(nums);
+        System.out.println("Input  : " + Arrays.toString(nums));
+        System.out.println("Output : " + res);
+
+        nums = new int[]{1};
+        res = sol.maxSubArray(nums);
+        System.out.println("Input  : " + Arrays.toString(nums));
+        System.out.println("Output : " + res);
+
+        nums = new int[]{5,4,-1,7,8};
+        res = sol.maxSubArray(nums);
+        System.out.println("Input  : " + Arrays.toString(nums));
+        System.out.println("Output : " + res);
+    }
+}
