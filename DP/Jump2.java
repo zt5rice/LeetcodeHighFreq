519040401a0aa41ffeb4617c7b4836785caac09b
Zhao Tang <zhao.tang@rice.edu>
2021-04-29 16:41:56 -0500

    Jump2M.java

--- DP/Jump2.java
+++ DP/Jump2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Jump2 {
-    public int jump(int[] nums) {
+    public int jump(int[] nums) { // Method: DP, TC: O(N2), sc: o(n)
         if (nums == null || nums.length < 2) {
             return 0;
         }
@@ -17,6 +17,21 @@ public class Jump2 {
         }
         System.out.println(Arrays.toString(dp));
         return dp[nums.length - 1];
+    }
+
+    public int jump2(int[] nums) { // greedy, TC: O(n), SC: o(1)
+        int jumps = 0, currentJumpEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            // we continuously find the how far we can reach in the current jump
+            farthest = Math.max(farthest, i + nums[i]);
+            // if we have come to the end of the current jump,
+            // we need to make another jump
+            if (i == currentJumpEnd) {
+                jumps++;
+                currentJumpEnd = farthest;
+            }
+        }
+        return jumps;
     }
     public static void main(String[] args) {
         Jump2 sol = new Jump2();
