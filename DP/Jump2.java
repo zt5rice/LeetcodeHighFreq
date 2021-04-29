5d630fc2a329c17f2e950b67644614605dea77db
Zhao Tang <zhao.tang@rice.edu>
2021-04-29 17:11:35 -0500

    Jump2.java

--- DP/Jump2.java
+++ DP/Jump2.java
@@ -33,6 +33,22 @@ public class Jump2 {
         }
         return jumps;
     }
+
+    public int jump3(int[] nums) { // variant BFS, tc: o(n), sc: o(1)
+        if (nums.length <= 1) return 0;
+        int curMax = 0; // to mark the last element in a level
+        int level = 0, i = 0;
+        while (i <= curMax) { 
+            int furthest = curMax; // to mark the last element in the next level
+            for (; i <= curMax; i++) {
+                furthest = Math.max(furthest, nums[i] + i);
+                if (furthest >= nums.length - 1) return level + 1;
+            }
+            level++;
+            curMax = furthest;
+        }
+        return -1; // if i < curMax, i can't move forward anymore (the last element in the array can't be reached)
+    }
     public static void main(String[] args) {
         Jump2 sol = new Jump2();
         int[] nums;
