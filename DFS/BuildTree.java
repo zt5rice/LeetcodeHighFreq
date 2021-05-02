a5b7aa21536bd7e68c629b9985b88d1679628596
Zhao Tang <zhao.tang@rice.edu>
2021-05-02 02:28:21 -0500

    BuildTree

---
+++ DFS/BuildTree.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class BuildTree {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++) map.put(inorder[i], i);
+        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
+    }
+    
+    public TreeNode buildTree(int[] preorder, int s1, int e1, int[] inorder, int s2, int e2, Map<Integer, Integer> map) {
+        if(s1 > e1) return null;
+        //if(s1 == e1) return new TreeNode(preorder[s1]);
+  
+        TreeNode root = new TreeNode(preorder[s1]);
+        
+        int m = map.get(root.val);
+        
+        root.left = buildTree(preorder, s1 + 1, s1 + m - s2, inorder, s2, m - 1, map);
+        root.right = buildTree(preorder, s1 + m - s2 + 1, e1, inorder, m + 1, e2, map);
+        
+        return root;
+    }     
+    
+    public static void main(String[] args) {
+        BuildTree sol = new BuildTree();
+        ConstructTree r = new ConstructTree();
+        int[] preorder, inorder;
+        TreeNode res;
+
+        preorder = new int[]{3,9,20,15,7};
+        inorder = new int[]{9,3,15,20,7};
+        res = sol.buildTree(preorder, inorder);
+        System.out.println("Result : " + Arrays.toString(r.destructTree(res).toString()));
+    }
+}
