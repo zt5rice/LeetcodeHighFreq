15fe48b9562ac478c5a7d614246ce009a80f48f8
Zhao Tang <zhao.tang@rice.edu>
2021-05-04 17:11:15 -0500

    count bits

---
+++ String/CountBits.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class CountBits {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 1; i <= num; ++i)
+          ans[i] = ans[i & (i - 1)] + 1;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        CountBits sol = new CountBits();
+        int num;
+        int[] res;
+
+        num = 2;
+        res = sol.countBits(num);
+        System.out.println("Input  : " + num);
+        System.out.println("Output : " + Arrays.toString(res));
+    }
+}
