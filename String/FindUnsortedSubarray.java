b7fafa5ddab5b0c6eb503fee337388a0bf384d53
Zhao Tang <zhao.tang@rice.edu>
2021-05-04 17:51:20 -0500

    find unsorted

---
+++ String/FindUnsortedSubarray.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class FindUnsortedSubarray {
+    public int findUnsortedSubarray(int[] nums) {
+        int len=nums.length;
+        int max=Integer.MIN_VALUE, min=Integer.MAX_VALUE;
+        int start=-1, end=-1;
+        
+        for(int i=0; i<len; i++){
+            max = Math.max(max, nums[i]); //from left to right, search the current max
+            min = Math.min(min, nums[len-i-1]);  //from right to left, search the current min 
+            
+            if(nums[i] < max)  
+                end = i;
+            if(nums[len-i-1] > min)
+                start = len-i-1;
+        }
+        
+        if(start==-1) //the entire array is already sorted
+            return 0;
+        
+        return end-start+1;
+    }
+    
+    public static void main(String[] args) {
+        FindUnsortedSubarray sol = new FindUnsortedSubarray();
+        int[] nums;
+        int res;
+
+        nums = new int[]{2,6,4,8,10,9,15};
+        res = sol.findUnsortedSubarray(nums);
+        System.out.println("Input  : " + Arrays.toString(nums));
+        System.out.println("Output : " + res);
+    }
+}
