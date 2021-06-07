101fe2dc7238ecfdc9453090de7769b5a1cd2d70
Zhao Tang <zhao.tang@rice.edu>
2021-06-06 21:34:05 -0500

    Rotate.java better

--- String/Rotate.java
+++ String/Rotate.java
@@ -20,36 +20,37 @@ public class Rotate {
                 matrix[i][n - j - 1] = tmp;
             }
         }
-    }        
+    }
+
     // best vesion by Yan
-        public void rotateBest(int[][] matrix) {
-            int n = matrix.length;
-            if (n <= 1) {
-              return;
-            }
-            int round = n / 2;
-            for (int level = 0; level < round; level++) {
-              int left = level;
-              int right = n - 2 - level;
-              for (int i  = left; i <= right; i++) {
+    public void rotateBest(int[][] matrix) {
+        int n = matrix.length;
+        if (n <= 1) {
+            return;
+        }
+        int round = n / 2;
+        for (int level = 0; level < round; level++) {
+            int left = level;
+            int right = n - 2 - level;
+            for (int i = left; i <= right; i++) {
                 int tmp = matrix[left][i];
                 matrix[left][i] = matrix[n - 1 - i][left];
                 matrix[n - 1 - i][left] = matrix[n - 1 - left][n - 1 - i];
                 matrix[n - 1 - left][n - 1 - i] = matrix[i][n - 1 - left];
                 matrix[i][n - 1 - left] = tmp;
-              }
             }
-          }
+        }
+    }
 
-        //
+    //
     public static void main(String[] args) {
         Rotate sol = new Rotate();
         int[][] matrix;
 
-        matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        matrix = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         System.out.println("Input  : " + Arrays.deepToString(matrix));
         sol.rotate(matrix);
-        System.out.println("Output : " + Arrays.deepToString(matrix));  
-        sol.rotateBest(matrix);      
+        System.out.println("Output : " + Arrays.deepToString(matrix));
+        sol.rotateBest(matrix);
     }
 }
