9028e2e49a93b27e32bda56967457c4bf614ca24
Zhao Tang <zhao.tang@rice.edu>
2021-06-08 12:54:35 -0500

    LowestCommonAncestor1.java

--- Tree/LowestCommonAncestor1.java
+++ Tree/LowestCommonAncestor1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class LowestCommonAncestor1 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q)  return root;
+        if(root == null || root.key == p.key || root.key == q.key)  return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         if(left != null && right != null)   return root;
@@ -19,12 +19,33 @@ public class LowestCommonAncestor1 {
         System.out.println("Input tree : " + Arrays.toString(rootint));
         System.out.println("p, q :" + p.key + " , " + q.key);
         if (res != null) {
-            System.out.println("Output LCA : " + res);
+            System.out.println("Output LCA : " + res.key);
         } else {
             System.out.println("Output LCA : null");
         }       
     }
 }
+/*
+method: recursion
+base case: 
+1) root == null  -> return null;
+2) rootLeft = lca(root.left, one, two);
+rootLeft = lca(root.left, one, two);
+
+
+        5
+
+      /   \
+
+     9()     12
+
+   /  \      \
+
+  (2)    3      (14)
+*/
+
 /*abstract
 To be improved: use search TreeNode find input node, then it will work.
+works: no duplicate key, check key equal, rather than treeNode equal
 abstract*/
+
