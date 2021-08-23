f6da102f026a8f619eecc3211dd190792dd34d7c
Zhao Tang <zhao.tang@rice.edu>
2021-08-23 00:16:04 -0500

    findMaximumXOR

---
+++ Trie/FindMaximumXOR.java
@@ -0,0 +1,69 @@
+public class FindMaximumXOR {
+    public int findMaximumXOR(int[] nums) {
+        int max = 0, mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask |= (1 << i);
+            HashSet<Integer> set = new HashSet<Integer>();
+            for (int num : nums) {
+                set.add(num & mask); // reserve Left bits and ignore Right bits
+            }
+            
+            /* Use 0 to keep the bit, 1 to find XOR
+             * 0 ^ 0 = 0 
+             * 0 ^ 1 = 1
+             * 1 ^ 0 = 1
+             * 1 ^ 1 = 0
+             */
+            int tmp = max | (1 << i); // in each iteration, there are pair(s) whoes Left bits can XOR to max
+            for (int prefix : set) {
+                if (set.contains(tmp ^ prefix)) {
+                    max = tmp;
+                }
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+/*abstract
+
+421. Maximum XOR of Two Numbers in an Array
+Medium
+
+Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 <= i <= j < n.
+
+ 
+
+Example 1:
+
+Input: nums = [3,10,5,25,2,8]
+Output: 28
+Explanation: The maximum result is 5 XOR 25 = 28.
+Example 2:
+
+Input: nums = [0]
+Output: 0
+Example 3:
+
+Input: nums = [2,4]
+Output: 6
+Example 4:
+
+Input: nums = [8,10,2]
+Output: 10
+Example 5:
+
+Input: nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+Output: 127
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 105
+0 <= nums[i] <= 231 - 1
+
+https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/discuss/91049/Java-O(n)-solution-using-bit-manipulation-and-HashMap
+
+*/
