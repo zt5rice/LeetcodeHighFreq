23dbd251aa8d4f5b6473f964447b6af73a5a2b2d
Zhao Tang <zhao.tang@rice.edu>
2021-08-24 17:22:24 -0500

    CopyRandomList.java

---
+++ List/CopyRandomList.java
@@ -0,0 +1,17 @@
+public class CopyRandomList {
+    Map<Node, Node> copied = new HashMap<>();
+    
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (this.copied.containsKey(head)) {
+            return this.copied.get(head); // why return null?
+        }    
+        Node newNode = new Node(head.val);
+        copied.put(head, newNode);
+        newNode.next = copyRandomList(head.next);
+        newNode.random = copyRandomList(head.random);
+        return newNode;
+    }
+}
