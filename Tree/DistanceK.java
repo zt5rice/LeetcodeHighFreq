6504fadbfbdf611b3249274c8139c41194e4a70b
Zhao Tang <zhao.tang@rice.edu>
2021-08-26 02:37:10 -0500

    Codec.java

---
+++ Tree/DistanceK.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class DistanceK {
+        
+    Map<TreeNode, Integer> map = new HashMap<>();
+        
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        List<Integer> res = new LinkedList<>();
+        find(root, target);
+        dfs(root, target, K, map.get(root), res);
+        return res;
+    }
+    
+    // find target node first and store the distance in that path that we could use it later directly
+    private int find(TreeNode root, TreeNode target) {
+        if (root == null) return -1;
+        if (root == target) {
+            map.put(root, 0);
+            return 0;
+        }
+        int left = find(root.left, target);
+        if (left >= 0) {
+            map.put(root, left + 1);
+            return left + 1;
+        }
+		int right = find(root.right, target);
+		if (right >= 0) {
+            map.put(root, right + 1);
+            return right + 1;
+        }
+        return -1;
+    }
+    
+    private void dfs(TreeNode root, TreeNode target, int K, int length, List<Integer> res) {
+        if (root == null) return;
+        if (map.containsKey(root)) length = map.get(root);
+        if (length == K) res.add(root.key);
+        dfs(root.left, target, K, length + 1, res);
+        dfs(root.right, target, K, length + 1, res);
+    }
+}
+/*abstract
+
+
+https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/discuss/143798/1ms-beat-100-simple-Java-dfs-with(without)-hashmap-including-explanation
+*/
