363acee1c0bdadb9aeb5c4ddaa909cc9786a3afe
Zhao Tang <zhao.tang@rice.edu>
2021-09-23 17:27:05 -0500

    MinCostII.java

---
+++ DP/MinCostII.java
@@ -0,0 +1,154 @@
+public class MinCostII {
+    public int minCostII0(int[][] costs) { // INTUITIVE
+
+        if (costs.length == 0) return 0;
+        int k = costs[0].length;
+        int n = costs.length;
+
+        for (int house = 1; house < n; house++) {
+            for (int color = 0; color < k; color++) {
+                int min = Integer.MAX_VALUE;
+                for (int previousColor = 0; previousColor < k; previousColor++) {
+                    if (color == previousColor) continue;
+                    min = Math.min(min, costs[house - 1][previousColor]);
+                }
+                costs[house][color] += min;
+            }
+        }
+
+        // Find the minimum in the last row.
+        int min = Integer.MAX_VALUE;
+        for (int c : costs[n - 1]) {
+            min = Math.min(min, c);
+        }
+        return min;
+    }
+
+    public int minCostII1(int[][] costs) { // OPTIMIZE TIME
+
+        if (costs.length == 0) return 0;
+        int k = costs[0].length;
+        int n = costs.length;
+
+        for (int house = 1; house < n; house++) {
+
+            // Find the minimum and second minimum color in the PREVIOUS row.
+            int minColor = -1; int secondMinColor = -1;
+            for (int color = 0; color < k; color++) {
+                int cost = costs[house - 1][color];
+                if (minColor == -1 || cost < costs[house - 1][minColor]) {
+                    secondMinColor = minColor;
+                    minColor = color;
+                } else if (secondMinColor == -1 || cost < costs[house - 1][secondMinColor]) {
+                    secondMinColor = color;
+                }
+            }
+
+            // And now calculate the new costs for the current row.
+            for (int color = 0; color < k; color++) {
+                if (color == minColor) {
+                    costs[house][color] += costs[house - 1][secondMinColor];
+                } else {
+                    costs[house][color] += costs[house - 1][minColor];
+                }
+            }
+        }
+
+        // Find the minimum in the last row.
+        int min = Integer.MAX_VALUE;
+        for (int c : costs[n - 1]) {
+            min = Math.min(min, c);
+        }
+        return min;
+    }
+
+    public int minCostII2(int[][] costs) { // OPTIMIZE TIME AND SPACE
+
+        if (costs.length == 0) return 0;
+        int k = costs[0].length;
+        int n = costs.length;
+
+
+        /* Firstly, we need to determine the 2 lowest costs of
+         * the first row. We also need to remember the color of
+         * the lowest. */
+        int prevMin = -1; int prevSecondMin = -1; int prevMinColor = -1;
+        for (int color = 0; color < k; color++) {
+            int cost = costs[0][color];
+            if (prevMin == -1 || cost < prevMin) {
+                prevSecondMin = prevMin;
+                prevMinColor = color;
+                prevMin = cost;
+            } else if (prevSecondMin == -1 || cost < prevSecondMin) {
+                prevSecondMin = cost;
+            }
+        }
+
+        // And now, we need to work our way down, keeping track of the minimums.
+        for (int house = 1; house < n; house++) {
+            int min = -1; int secondMin = -1; int minColor = -1;
+            for (int color = 0; color < k; color++) {
+                // Determine the cost for this cell (without writing it in).
+                int cost = costs[house][color];
+                if (color == prevMinColor) {
+                    cost += prevSecondMin;
+                } else {
+                    cost += prevMin;
+                }
+                // Determine whether or not this current cost is also a minimum.
+                if (min == -1 || cost < min) {
+                    secondMin = min;
+                    minColor = color;
+                    min = cost;
+                } else if (secondMin == -1 || cost < secondMin) {
+                    secondMin = cost;
+                }
+            }
+            // Transfer current mins to be previous mins.
+            prevMin = min;
+            prevSecondMin = secondMin;
+            prevMinColor = minColor;
+        }
+
+        return prevMin;
+    }
+}
+/*abstract
+265. Paint House II
+Hard
+
+There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+
+The cost of painting each house with a certain color is represented by an n x k cost matrix costs.
+
+For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on...
+Return the minimum cost to paint all houses.
+
+ 
+
+Example 1:
+
+Input: costs = [[1,5,3],[2,9,4]]
+Output: 5
+Explanation:
+Paint house 0 into color 0, paint house 1 into color 2. Minimum cost: 1 + 4 = 5; 
+Or paint house 0 into color 2, paint house 1 into color 0. Minimum cost: 3 + 2 = 5.
+Example 2:
+
+Input: costs = [[1,3],[2,4]]
+Output: 5
+ 
+
+Constraints:
+
+costs.length == n
+costs[i].length == k
+1 <= n <= 100
+2 <= k <= 20
+1 <= costs[i][j] <= 20
+ 
+
+Follow up: Could you solve it in O(nk) runtime?
+
+
+*/
