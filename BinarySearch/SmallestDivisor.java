b791e70cea96959a7836506b73e207dc6b7bb921
Zhao Tang <zhao.tang@rice.edu>
2021-10-28 14:09:19 -0500

    LeftMostColumnWithOne.java

--- BinarySearch/SmallestDivisor.java
+++ BinarySearch/SmallestDivisor.java
@@ -13,6 +13,7 @@ public class SmallestDivisor {
         return left;
     }    
 }
+
 /*abstract
 Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
 
